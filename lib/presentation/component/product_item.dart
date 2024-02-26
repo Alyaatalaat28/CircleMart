0f3901e3d86e4a4e10271caf7acab936dbcbf863
AlyaaTalaat <alyaatalaat205@gmail.com>
2024-02-26 19:07:14 +0200

    components

---
+++ lib/presentation/component/product_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/widgets.dart';
+import 'package:gap/gap.dart';
+import 'package:nami/core/resources/colors.dart';
+import 'package:nami/core/resources/app_styles.dart';
+import '../modules/home/widgets/product_price_and_cart.dart';
+import '../modules/home/widgets/product_stack.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+          constraints: const BoxConstraints(
+            maxWidth: double.infinity, 
+            maxHeight: double.infinity, 
+          ),
+          child: Container(
+            decoration: ShapeDecoration(
+              color: AppColors.kLightGray,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+            child: AspectRatio(
+              aspectRatio: 2 / 2,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Column(
+                  crossAxisAlignment:CrossAxisAlignment.end,
+                  children: [
+                   const ProductStack(),
+                    const Spacer(),
+                    Text('جمبري',
+                    style:AppStyles.regular14(context,AppColors.kBlack)),
+                    const Gap(8),
+                    const ProductPriceAndCart(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+  }
+}
---
+++ lib/presentation/component/products_app_bar.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gap/gap.dart';
+import 'package:nami/core/extensions/num_extension.dart';
+import 'package:nami/core/resources/app_styles.dart';
+import 'package:nami/core/resources/assets.dart';
+
+class ProductsAppBar extends StatelessWidget {
+  const ProductsAppBar({super.key,required this.text});
+ final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Row(
+        children:[
+          const Spacer(),
+          Text(text,
+          style:AppStyles.semiBold18(context)
+          ),
+          Gap(10.w),
+          InkWell(
+            onTap: (){
+              Navigator.pop(context);
+            },
+            child: SvgPicture.asset(Assets.arrow))
+        ]
+      ),
+    );
+  }
+}
