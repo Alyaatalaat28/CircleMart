b4f41c30f88a3a6b1e657161d5ceff92d364cb19
AlyaaTalaat <alyaatalaat205@gmail.com>
2024-02-26 19:11:43 +0200

    custom bottom sheet

---
+++ lib/presentation/sheet/bottom_sheet.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:nami/core/extensions/num_extension.dart';
+import 'package:nami/core/resources/app_styles.dart';
+import 'package:nami/core/resources/colors.dart';
+
+class CustomBottomSheet extends StatelessWidget {
+  const CustomBottomSheet({super.key,
+  required this.width,required this.height,
+  required this.child,required this.text});
+  
+ final Widget child;
+ final double width;
+ final double height;
+ final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomSheet(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      elevation: 1.0,
+      builder: (BuildContext context) => Padding(
+        padding: const EdgeInsets.symmetric(horizontal:16.0,vertical:12.0),
+        child: Center(
+          child: Row(
+            children:[
+              Container(
+                width: width.w,
+                height: height.h,
+                decoration:ShapeDecoration(
+              color:AppColors.kRed,
+              shape:RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+            ),
+                child:child,
+              ),
+              const Spacer(),
+              Text('ج.م',
+              style:AppStyles.regular12(context, AppColors.kBlack)),
+              Gap(4.w),
+              Text(text,
+              style:AppStyles.semiBold18(context)),
+              Gap(4.w),
+              Text(': الاجمالي',
+              style:AppStyles.regular12(context, AppColors.kBlack))
+            ]
+          ),
+        ),
+      ),
+       onClosing: () {  },);
+  }
+}
